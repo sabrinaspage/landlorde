c0ae67a5dea8b0f2dcf8874934799d547c7549a1
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2023-05-16 18:28:59 -0400

    database types

---
+++ landlorde_backend/src/types.ts
@@ -0,0 +1,21 @@
+enum AdaAccess {
+  ACCESSIBLE = "accessible",
+  INACCESSIBLE = "inaccessible",
+  PARTIALLY_ACCESSIBLE = "partiallyAccessible",
+}
+
+enum RequestStatus {
+  NEW = "new",
+  OVERDUE = "overdue",
+  VIEWED = "viewed",
+  WONT_DO = "wontDo",
+  SOLVED = "solved",
+  PENDING = "pending",
+  POSTPONED = "postponed",
+}
+
+enum RequestUrgency {
+  EMERGENCY = "emergency",
+  URGENT = "urgent",
+  ROUTINE = "routine",
+}
