c444373c4d7da43484bb969bdcad07a1c6684e6a
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2023-05-17 02:28:56 -0400

    add CRUD methods to Unit controller

--- landlorde_backend/src/controllers/unit.ts
+++ landlorde_backend/src/controllers/unit.ts
@@ -7,6 +7,79 @@ class UnitController {
   constructor() {
     this.pool = pool;
   }
+
+  async getAllUnits() {
+    const client = await pool.connect();
+    try {
+      const result = await client.query("SELECT * FROM unit");
+      return result.rows;
+    } finally {
+      client.release();
+    }
+  }
+
+  async getUnitById(unitId: string) {
+    const client = await pool.connect();
+    try {
+      const result = await client.query("SELECT * FROM unit WHERE id = $1", [
+        unitId,
+      ]);
+      return result.rows[0];
+    } finally {
+      client.release();
+    }
+  }
+
+  async createUnit(unitData: any) {
+    const client = await this.pool.connect();
+
+    try {
+      const { unit_number } = unitData;
+      const result = await client.query(
+        `
+          INSERT INTO unit (unitnumber)
+          SELECT $1, $2
+          FROM inserted_contactinfo
+          RETURNING *;
+        `,
+        [unit_number]
+      );
+      return result.rows[0];
+    } finally {
+      client.release();
+    }
+  }
+
+  async updateUnitById(unitName: string | null, unitId: string) {
+    const client = await pool.connect();
+    try {
+      const result = await client.query(
+        `
+        UPDATE unit SET
+          unitnumber = COALESCE($2, unitnumber)
+          WHERE id = $1
+        RETURNING *;
+        `,
+        [unitId, unitName]
+      );
+      return result.rows[0];
+    } finally {
+      client.release();
+    }
+  }
+
+  async deleteUnit(unitId: string) {
+    const client = await this.pool.connect();
+
+    try {
+      const result = await client.query("DELETE FROM unit WHERE id = $1", [
+        unitId,
+      ]);
+      return result.rowCount;
+    } finally {
+      client.release();
+    }
+  }
 }
 
 export default UnitController;
