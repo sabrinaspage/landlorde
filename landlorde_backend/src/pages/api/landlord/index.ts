2c3be3fe998c5ee70b4b799ddcffa839521b9db1
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2023-05-21 00:03:09 -0400

    get all landlords handler

---
+++ landlorde_backend/src/pages/api/landlord/index.ts
@@ -0,0 +1,18 @@
+import landlord from "@/controllers/landlord";
+import { NextApiRequest, NextApiResponse } from "next";
+import { DatabaseError } from "pg";
+
+export default async function landlordsHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const result = await landlord.getAllLandlords();
+
+  if (result instanceof DatabaseError) {
+    res.status(200).json({ message: result.message });
+    return;
+  }
+
+  res.status(200).json({ message: result.rows });
+  return;
+}
