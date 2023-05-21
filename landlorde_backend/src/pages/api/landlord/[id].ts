94ed7a421a9b9405d478117cc348877be8336484
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2023-05-21 01:22:29 -0400

    add delete method

--- landlorde_backend/src/pages/api/landlord/[id].ts
+++ landlorde_backend/src/pages/api/landlord/[id].ts
@@ -30,6 +30,32 @@ export default async function landlordHandler(
     res
       .status(400)
       .json({ message: `Landlord with id: ${id} does not exist.` });
+  if (req.method === "DELETE") {
+    const { query } = req;
+    const { id } = query;
+
+    if (!id) return;
+
+    if (id instanceof Array) {
+      res.status(400).json({ message: `Only one id allowed.` });
+      return;
+    }
+
+    const result = await landlord.deleteLandlord(id);
+
+    if (result instanceof DatabaseError) {
+      res.status(200).json({ message: result.message });
+      return;
+    }
+
+    if (result.rowCount === 0) {
+      res
+        .status(400)
+        .json({ message: `Landlord with id: ${id} does not exist.` });
+      return;
+    }
+
+    res.status(200).json({ message: `Successfully deleted ${id}.` });
     return;
   }
 
